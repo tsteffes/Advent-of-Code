e83ab835478d40bd78b1b3a91a9a126b88260f21
tsteffes <tim.steffes@gmail.com>
2020-12-03 09:44:05 -0500

    cleanup

--- Puzzles/Day-3/solution.js
+++ Puzzles/Day-3/solution.js
@@ -4,9 +4,9 @@ const inputFile = 'Puzzles/Day-3/input.txt';
 const partOneConfigs = [ { rise: 1, run: 3 } ];
 const partTwoConfigs = [{ rise: 1, run: 1 }, { rise: 1, run: 3 }, { rise: 1, run: 5 }, { rise: 1, run: 7 }, { rise: 2, run: 1 }];
 
-let calculateCollisions = (map, rise, run) => {
+let calculateCollisions = (map, config) => {
   let positions = [];
-  for (let yPos = 0, xPos = 0; yPos < map.length; yPos += rise, xPos = (xPos + run) % map[0].length) {
+  for (let yPos = 0, xPos = 0; yPos < map.length; yPos += config.rise, xPos = (xPos + config.run) % map[0].length) {
     positions.push([xPos, yPos]);
   }
 
@@ -16,7 +16,7 @@ let calculateCollisions = (map, rise, run) => {
 let getSolution = (map, configs) => {
   let result = 1;
   _.forEach(configs, c => {
-    result *= calculateCollisions(map, c.rise, c.run);
+    result *= calculateCollisions(map, c);
   });
 
   return result;
