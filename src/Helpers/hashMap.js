190b639a3686c2764cf033fedd968a986875dc8e
tsteffes <tim.steffes@gmail.com>
2020-12-15 06:44:07 -0500

    refactor

---
+++ src/Helpers/hashMap.js
@@ -0,0 +1,14 @@
+class HashMap {
+  constructor(bucketCount) {
+    this.buckets = [];
+    for (var i = 0; i < bucketCount; i++) {
+      this.buckets.push({});
+    }
+  }
+
+  hash = k => k % this.buckets.length;
+  set = (k, v) => this.buckets[this.hash(k)][k] = v;
+  get = k => this.buckets[this.hash(k)][k];
+}
+
+exports.HashMap = HashMap;
--- src/Puzzles/Day-15/solution.js
+++ src/Puzzles/Day-15/solution.js
@@ -1,22 +1,12 @@
 const _ = require('lodash');
 const io = require('../../Helpers/io');
 const Solver = require('../../Helpers/solver').Solver;
+const HashMap = require('../../Helpers/hashMap').HashMap;
 
 let getValues = input => _.map(input[0].split(','), i => parseInt(i));
 
-function HashMap() {
-  this.buckets = [];
-  for (var i = 0; i < 32768; i++) {
-    this.buckets.push({});
-  }
-
-  this.hash = k => k % 32768;
-  this.set = (k, v) => this.buckets[this.hash(k)][k] = v;
-  this.get = k => this.buckets[this.hash(k)][k];
-}
-
 let getSolution = (input, config, part) => {
-  let indexes = new HashMap();
+  let indexes = new HashMap(32768);
   input.slice(0, input.length - 1).forEach(i => indexes.set(i, input.lastIndexOf(i)));
   let num = input[input.length - 1];
   for (let i = input.length; i < config.target; i++) {
