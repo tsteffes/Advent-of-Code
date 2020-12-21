152e95b06882284b7fca91cf814e173cad1761b4
tsteffes <tim.steffes@gmail.com>
2020-12-21 04:15:46 -0500

    initialize all days

---
+++ src/Puzzles/Day-24/solution.js
@@ -0,0 +1,14 @@
+const _ = require('lodash');
+const io = require('../../Helpers/io');
+const Solver = require('../../Helpers/solver').Solver;
+
+let getValues = input => input;
+
+let getSolution = (input, config, part) => {
+
+};
+
+new Solver(24, io.readLines, getValues, getSolution, [{ }]).solve();
+
+// Part 1 solution:
+// Part 2 solution:
