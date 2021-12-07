1bfaaf72f779e507daf8c411a3035f7c97f2a7de
tsteffes <tim.steffes@gmail.com>
2021-12-07 16:44:41 -0500

    day-07

---
+++ src/Puzzles/2021/Day-07/solution.js
@@ -0,0 +1,34 @@
+const _ = require('lodash');
+const io = require('../../../Helpers/io');
+const Solver = require('../../../Helpers/solver').Solver;
+
+let getValues = input => {
+  return input[0].split(',').map(v => parseInt(v, 10));
+};
+
+let getCost = (i, val, config) => {
+  let dist = Math.abs(i - val);
+  if (config.part === 1) {
+    return dist;
+  }
+
+  return (dist * (dist + 1)) / 2;
+}
+
+let getSolution = (values, config) => {
+  let max = _.max(values);
+  let res = null;
+  for (let i = 0; i < max; i++) {
+    let tot = _.sum(values.map(v => getCost(i, v, config)));
+    if (res === null || tot < res) {
+      res = tot;
+    }
+  }
+
+  return res;
+};
+
+new Solver(2021, 7, io.readLines, getValues, getSolution, [{ part: 1 }, { part: 2 }]).solve();
+
+// Part 1 solution: 329389
+// Part 2 solution: 86397080
