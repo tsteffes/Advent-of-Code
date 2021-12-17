aa3df67d352cbe6913291316c91b728399815c94
tsteffes <tim.steffes@gmail.com>
2021-12-17 11:40:29 -0500

    day-13

---
+++ src/Puzzles/2021/Day-13/solution.js
@@ -0,0 +1,47 @@
+const _ = require('lodash');
+const io = require('../../../Helpers/io');
+const Solver = require('../../../Helpers/solver').Solver;
+
+let getValues = input => {
+  let res = { coords: [], folds: [] };
+  for (line of input) {
+    let c = line.match(/(?<x>[\d]+),(?<y>[\d]+)/);
+    let f = line.match(/fold along (?<dir>[xy])=(?<val>[\d]+)/);
+    if (c) {
+      res.coords.push({ x: parseInt(c.groups.x, 10), y: parseInt(c.groups.y, 10) });
+    }
+    else if (f) {
+      res.folds.push({ dir: f.groups.dir, val: parseInt(f.groups.val, 10)});
+    }
+  }
+
+  return res;
+};
+
+let getSolution = (input, config) => {
+  for (let fold of _.take(input.folds, config.part === 1 ? 1 : input.folds.length)) {
+    for (let coord of input.coords) {
+      coord.x = fold.dir === 'x' && coord.x > fold.val ? (fold.val - (coord.x - fold.val)) : coord.x;
+      coord.y = fold.dir === 'y' && coord.y > fold.val ? (fold.val - (coord.y - fold.val)) : coord.y;
+    }
+  }
+
+  if (config.part === 1) {
+    return _.uniqBy(input.coords.map(v => v.x + ',' + v.y)).length;
+  }
+
+  for (let j = 0; j <= _.max(input.coords.map(c => c.y)); j++) {
+    let line = '';
+    for (let i = 0; i <= _.max(input.coords.map(c => c.x)); i++) {
+      let f = _.filter(input.coords, c => c.x === i && c.y === j);
+      line += f.length > 0 ? '#' : '.';
+    }
+
+    console.log(line);
+  }
+};
+
+new Solver(2021, 13, io.readLines, getValues, getSolution, [{ part: 1 }, { part: 2 }]).solve();
+
+// Part 1 solution: 669
+// Part 2 solution: UEFZCUCJ
