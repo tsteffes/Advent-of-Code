9434d0c5f7f9a68e1836a362d3a7d384873a5759
Tim Steffes <tsteffes@plex.com>
2022-12-01 00:22:27 -0500

    day 1

---
+++ src/Puzzles/2022/Day-01/solution.js
@@ -0,0 +1,16 @@
+const _ = require('lodash');
+const io = require('../../../Helpers/io');
+const Solver = require('../../../Helpers/solver').Solver;
+
+let getValues = input => {
+  return input.map(i => _.map(i.split('\r\n'), v => parseInt(v)));
+};
+
+let getSolution = (values, config) => {
+  return _.sum(_.take(_.reverse(_.sortBy(_.map(values, v => _.sum(v)))), config.top));
+};
+
+new Solver(2022, 1, i => io.readLines(i, '\r\n\r\n'), getValues, getSolution, [{ top: 1 }, { top: 3 }]).solve();
+
+// Part 1 solution: 69501
+// Part 2 solution: 202346
