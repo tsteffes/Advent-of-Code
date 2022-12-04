016d65b4cc65460c20778cfdd4555e357b23b259
tsteffes <tim.steffes@gmail.com>
2022-12-04 16:00:09 -0500

    init

--- src/Helpers/solver.js
+++ src/Helpers/solver.js
@@ -3,7 +3,7 @@ const log = results => results.forEach((r, i) => {
 });
 
 exports.solve = (inputter, mapper, method, configs = []) => {
-  const file = process.argv[1].match(/(?<folder>.*)solution\.js/).groups.folder + 'input.txt';
+  const file = process.argv[1].match(/(?<f>.*)solution\.js/).groups.f + 'input.txt';
   configs = [{ part: 1, ...configs[0] }, { part: 2, ...configs[1] }];
   log(configs.map(c => method(mapper(inputter(file), c), c)));
 };
---
+++ src/Puzzles/2022/day-17/solution.js
@@ -0,0 +1,15 @@
+const _ = require('lodash');
+const io = require('../../../helpers/io');
+const Solver = require('../../../helpers/solver');
+
+let getValues = input => {
+};
+
+let getSolution = (input, config) => {
+  return
+};
+
+Solver.solve(io.readLines, getValues, getSolution);
+
+// Part 1 solution:
+// Part 2 solution:
