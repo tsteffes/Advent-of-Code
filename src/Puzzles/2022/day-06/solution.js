3ec99a659f98b70dd8fd4d2f8175f8d1c6201e94
tsteffes <tim.steffes@gmail.com>
2022-12-06 08:40:47 -0500

    day 6

--- src/Puzzles/2022/day-06/solution.js
+++ src/Puzzles/2022/day-06/solution.js
@@ -2,14 +2,15 @@ const _ = require('lodash');
 const io = require('../../../helpers/io');
 const Solver = require('../../../helpers/solver');
 
-let getValues = input => {
-};
-
 let getSolution = (input, config) => {
-  return
+  for (let i = 0; i < input.length - config.num; i++) {
+    if (_.uniq(input.substring(i, i + config.num).split('')).length === config.num) {
+      return i + config.num;
+    }
+  }
 };
 
-Solver.solve(io.readLines, getValues, getSolution);
+Solver.solve(io.readLines, i => i[0], getSolution, [{ num: 4 }, { num: 14 }]);
 
-// Part 1 solution:
-// Part 2 solution:
+// Part 1 solution: 1080
+// Part 2 solution: 3645
