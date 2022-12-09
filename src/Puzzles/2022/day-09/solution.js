44dd30b74510f9bb9de99c1839c6bd965a80369b
tsteffes <tim.steffes@gmail.com>
2022-12-09 09:43:27 -0500

    day 9

--- src/Puzzles/2022/day-09/solution.js
+++ src/Puzzles/2022/day-09/solution.js
@@ -2,13 +2,40 @@ const _ = require('lodash');
 const io = require('../../../helpers/io');
 const Solver = require('../../../helpers/solver');
 
-const getValues = input => input;
-
-const getSolution = (input, config) => {
-  return input;
+const dirs = { R: [1, 0], L: [-1, 0], U: [0, 1], D: [0, -1] };
+const getValues = input => {
+  return _.forEach(input.map(i => i.match(/(?<dir>[RLUD])\s(?<dist>\d+)/).groups), m => m.dist = parseInt(m.dist));
 };
 
-Solver.solve(io.readLines, getValues, getSolution);
+const getSolution = (input, config) => {
+  const state = { locs: [], segs: [] };
+  _.times(config.len + 1, () => state.segs.push([0, 0]));
+  input.forEach(move => {
+    _.times(move.dist, () => {
+      state.segs[0][0] += dirs[move.dir][0];
+      state.segs[0][1] += dirs[move.dir][1];
 
-// Part 1 solution:
-// Part 2 solution:
+      for (let seg = 0; seg < config.len; seg++) {
+        let seg1 = state.segs[seg];
+        let seg2 = state.segs[seg + 1];
+        if (Math.abs(seg1[0] - seg2[0]) + Math.abs(seg1[1] - seg2[1]) === 3) {
+          seg2[0] += seg2[0] > seg1[0] ? -1 : 1;
+          seg2[1] += seg2[1] > seg1[1] ? -1 : 1;
+        }
+        else {
+          seg2[0] += Math.abs(seg1[0] - seg2[0]) === 2 ? (seg1[0] > seg2[0] ? 1 : -1) : 0;
+          seg2[1] += Math.abs(seg1[1] - seg2[1]) === 2 ? (seg1[1] > seg2[1] ? 1 : -1) : 0;
+        }
+      }
+
+      state.locs.push(state.segs[config.len][0] + ',' + state.segs[config.len][1]);
+    });
+  });
+
+  return _.uniq(state.locs).length;
+};
+
+Solver.solve(io.readLines, getValues, getSolution, [ { len: 1 }, { len: 9 }]);
+
+// Part 1 solution: 5695
+// Part 2 solution: 2434
