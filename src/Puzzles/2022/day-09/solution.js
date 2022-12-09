a35b360d3d259e15d6b80875ea9775cad944671a
tsteffes <tim.steffes@gmail.com>
2022-12-09 13:16:17 -0500

    refactor

--- src/Puzzles/2022/day-09/solution.js
+++ src/Puzzles/2022/day-09/solution.js
@@ -2,36 +2,32 @@ const _ = require('lodash');
 const io = require('../../../helpers/io');
 const Solver = require('../../../helpers/solver');
 
-const dirs = { R: [1, 0], L: [-1, 0], U: [0, 1], D: [0, -1] };
-const getValues = input => input.map(i => i.match(/(?<dir>[RLUD])\s(?<dist>\d+)/).groups);
+const directions = { R: [1, 0], L: [-1, 0], U: [0, 1], D: [0, -1] };
+const getValues = input => input.map(i => i.match(/(?<direction>[RLUD])\s(?<distance>\d+)/).groups);
 const getSolution = (input, config) => {
-  const state = { locs: [], segs: [] };
-  _.times(config.len + 1, () => state.segs.push([0, 0]));
+  const rope = [], visited = [];
+  _.times(config.knots, () => rope.push({ x: 0, y: 0 }));
   input.forEach(move => {
-    _.times(parseInt(move.dist), () => {
-      state.segs[0][0] += dirs[move.dir][0];
-      state.segs[0][1] += dirs[move.dir][1];
-      for (let i = 0; i < config.len; i++) {
-        const seg1 = state.segs[i];
-        const seg2 = state.segs[i + 1];
-        if (Math.abs(seg1[0] - seg2[0]) + Math.abs(seg1[1] - seg2[1]) === 3) {
-          seg2[0] += seg2[0] > seg1[0] ? -1 : 1;
-          seg2[1] += seg2[1] > seg1[1] ? -1 : 1;
-        }
-        else {
-          seg2[0] += Math.abs(seg1[0] - seg2[0]) === 2 ? (seg1[0] > seg2[0] ? 1 : -1) : 0;
-          seg2[1] += Math.abs(seg1[1] - seg2[1]) === 2 ? (seg1[1] > seg2[1] ? 1 : -1) : 0;
-        }
+    _.times(parseInt(move.distance), () => {
+      rope[0].x += directions[move.direction][0];
+      rope[0].y += directions[move.direction][1];
+      for (let i = 0; i < config.knots - 1; i++) {
+        const xDiff = rope[i].x - rope[i + 1].x;
+        const yDiff = rope[i].y - rope[i + 1].y;
+        rope[i + 1].x += Math.abs(xDiff) + Math.abs(yDiff) === 3 ? (xDiff > 0 ? 1 : -1) :
+                         Math.abs(xDiff) === 2 ? (xDiff > 0 ? 1 : -1) : 0;
+        rope[i + 1].y += Math.abs(xDiff) + Math.abs(yDiff) === 3 ? (yDiff > 0 ? 1 : -1) :
+                         Math.abs(yDiff) === 2 ? (yDiff > 0 ? 1 : -1) : 0;
       }
 
-      state.locs.push(state.segs[config.len][0] + ',' + state.segs[config.len][1]);
+      visited.push(rope[config.knots - 1].x + ',' + rope[config.knots - 1].y);
     });
   });
 
-  return _.uniq(state.locs).length;
+  return _.uniq(visited).length;
 };
 
-Solver.solve(io.readLines, getValues, getSolution, [ { len: 1 }, { len: 9 }]);
+Solver.solve(io.readLines, getValues, getSolution, [ { knots: 2 }, { knots: 10 }]);
 
 // Part 1 solution: 5695
 // Part 2 solution: 2434
