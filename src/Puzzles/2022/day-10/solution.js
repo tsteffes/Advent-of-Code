0b7961d0d4a35275baa77b2f69f34bd8c04fdfa5
tsteffes <tim.steffes@gmail.com>
2022-12-10 09:04:05 -0500

    day 10

--- src/Puzzles/2022/day-10/solution.js
+++ src/Puzzles/2022/day-10/solution.js
@@ -2,13 +2,33 @@ const _ = require('lodash');
 const io = require('../../../helpers/io');
 const Solver = require('../../../helpers/solver');
 
-const getValues = input => input;
-
+const getValues = input => input.map(i => i.match(/(?<op>(addx|noop))\s?(?<val>[-\d]+)?/).groups);
+const render = (state) => {
+  state.row.push(Math.abs((state.cycle % 40) - state.X) < 2 ? '#' : '.');
+  if (state.cycle % 40 === 39) {
+    console.log(state.row.join(''));
+    state.row = [];
+  }
+};
 const getSolution = (input, config) => {
-  return input;
+  const state = { cycle: 0, X: 1, result: 0, row: [] };
+  input.forEach(instruction => {
+    _.times(instruction.op === 'noop' ? 1 : 2, () => {
+      if (config.part === 2) {
+        render(state);
+      }
+
+      state.cycle++;
+      state.result += state.cycle % 40 === 20 ? state.cycle * state.X : 0;
+    });
+
+    state.X += parseInt(instruction.val) || 0;
+  });
+
+  return state.result;
 };
 
 Solver.solve(io.readLines, getValues, getSolution);
 
-// Part 1 solution:
-// Part 2 solution:
+// Part 1 solution: 15680
+// Part 2 solution: ZFBFHGUP
