eda31a86f80656da4ee6dc9e8419819ea945efd6
tsteffes <tim.steffes@gmail.com>
2022-12-12 16:11:48 -0500

    day 12

--- src/Puzzles/2022/day-12/solution.js
+++ src/Puzzles/2022/day-12/solution.js
@@ -2,13 +2,56 @@ const _ = require('lodash');
 const io = require('../../../helpers/io');
 const Solver = require('../../../helpers/solver');
 
-const getValues = input => input;
+const dirs = [[0, -1], [-1, 0], [1, 0], [0, 1]];
+const getValues = input => {
+  const res = { start: null, map: [], lowPoints: [] };
+  for (let y = 0; y < input.length; y++) {
+    const row = [];
+    for (let x = 0; x < input[y].length; x++) {
+      const val = input[y][x].charCodeAt();
+      const point = { x: x, y: y, val: val === 69 ? 123 : val === 83 ? 96 : val, steps: null };
+      row.push(point);
+      res.end = val === 69 ? point : res.end;
+      res.start = val === 83 ? point : res.start;
+      val === 97 ? res.lowPoints.push(point) : null;
+    }
+
+    res.map.push(row);
+  }
+
+  res.end.steps = 0;
+  return res;
+};
 
 const getSolution = (input, config) => {
-  return input;
+  const stack = [input.end];
+  const map = input.map;
+  while (stack.length > 0) {
+    const p = stack.pop();
+    dirs.forEach(d => {
+      const x = p.x + d[0];
+      const y = p.y + d[1];
+      if (x >= 0 && x < map[0].length && y >= 0 && y < map.length) {
+        const neighbor = map[y][x];
+        if (p.val - neighbor.val <= 1) {
+          const newSteps = neighbor.steps === null ? p.steps + 1 : Math.min(neighbor.steps, p.steps + 1);
+          if (neighbor.steps === null || newSteps < neighbor.steps) {
+            stack.push(neighbor);
+            neighbor.steps = newSteps;
+          }
+        }
+      }
+    });
+  };
+
+  if (config.part === 1) {
+    return input.start.steps;
+  }
+
+  return _.sortBy(input.lowPoints.map(p => p.steps))[0];
 };
 
 Solver.solve(io.readLines, getValues, getSolution);
 
-// Part 1 solution:
-// Part 2 solution:
+// Part 1 solution: 456
+// Part 2 solution: 454
