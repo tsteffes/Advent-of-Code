a5145893a26eae770421a7a1a130b135778fe8c0
tsteffes <tim.steffes@gmail.com>
2022-12-16 09:52:07 -0500

    use the built-in json parser...

--- src/Puzzles/2022/day-13/solution.js
+++ src/Puzzles/2022/day-13/solution.js
@@ -4,51 +4,25 @@ const Solver = require('../../../helpers/solver');
 
 const getValues = (input, config) => {
   config.additionalPackets && input.push(config.additionalPackets.join('\r\n'));
-  const parseItem = (item, tokens) => {
-    const reg = /(?<inner>\[(?:(?:\$\d+|\d+|,)*)\])/g;
-    const inners = [...item.matchAll(reg)].map(x => x.groups.inner);
-    inners.forEach(i => {
-      const a = [];
-      let sub = i.substring(1, i.length - 1);
-      if (sub) {
-        a.push(...sub.split(','))
-      }
-
-      tokens.push(a);
-      item = item.replace(i, '$' + (tokens.length - 1));
-    });
-
-    if (!item.startsWith('[') || !item.endsWith(']')) {
-      return item;
-    }
-
-    return parseItem(item, tokens);
-  };
-  return input.map(i => i.split('\r\n').map(r => {
-    let orig = r;
-    let tokens = [];
-    parseItem(r, tokens);
-    return { orig, tokens };
-  }));
+  return input.map(i => i.split('\r\n').map(r => JSON.parse(r)));
 };
 
-const isToken = s => s.startsWith('$');
-const isOrdered = (a, aTokens, b, bTokens) => {
+const isOrdered = (a, b) => {
   for (let i = 0; i < a.length && i < b.length; i++) {
-    if (!isToken(a[i]) && !isToken(b[i])) {
-      if (parseInt(a[i]) < parseInt(b[i])) {
+    if (!Array.isArray(a[i]) && !Array.isArray(b[i])) {
+      if (a[i] < b[i]) {
         return 1;
       }
-      else if (parseInt(a[i]) > parseInt(b[i])) {
+      else if (a[i] > b[i]) {
         return -1;
       }
 
       continue;
     }
 
-    let left = isToken(a[i]) ? aTokens[a[i].substring(1)] : [a[i]];
-    let right = isToken(b[i]) ? bTokens[b[i].substring(1)] : [b[i]];
-    let res = isOrdered(left, aTokens, right, bTokens);
+    let left = Array.isArray(a[i]) ? a[i] : [a[i]];
+    let right = Array.isArray(b[i]) ? b[i] : [b[i]];
+    let res = isOrdered(left, right);
     if (res) return res;
   }
 
@@ -59,9 +33,7 @@ const getSolution = (input, config) => {
   if (config.part === 1) {
     let res = 0;
     for (let i = 0; i < input.length; i++) {
-      let left = input[i][0];
-      let right = input[i][1];
-      if (isOrdered(left.tokens[left.tokens.length - 1], left.tokens, right.tokens[right.tokens.length - 1], right.tokens) === 1) {
+      if (isOrdered(input[i][0], input[i][1]) === 1) {
         res += i + 1;
       }
     }
@@ -73,9 +45,9 @@ const getSolution = (input, config) => {
     arr.push(i[0], i[1]);
     return arr;
   }, []).sort((a, b) => {
-    return isOrdered(b.tokens[b.tokens.length - 1], b.tokens, a.tokens[a.tokens.length -1], a.tokens);
+    return isOrdered(b, a);
   });
-  let indices = _.keys(_.pickBy(orderedPackets, (p => config.additionalPackets.includes(p.orig))));
+  let indices = _.keys(_.pickBy(orderedPackets, (p => config.additionalPackets.includes(JSON.stringify(p)))));
   return indices.reduce((a, b) => (parseInt(a) + 1) * (parseInt(b) + 1));
 };
 
