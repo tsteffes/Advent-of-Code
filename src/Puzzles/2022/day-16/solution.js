62e567a334c55418cfb79c1c123134dbdbfe4631
tsteffes <tim.steffes@gmail.com>
2022-12-17 09:01:08 -0500

    wip

--- src/Puzzles/2022/day-16/solution.js
+++ src/Puzzles/2022/day-16/solution.js
@@ -2,10 +2,22 @@ const _ = require('lodash');
 const io = require('../../../helpers/io');
 const Solver = require('../../../helpers/solver');
 
-const getValues = input => input;
+const getValues = input => {
+  const reg = /Valve (?<name>[A-Z]+) has flow rate=(?<rate>\d+); tunnels? leads? to valves? (?<paths>.*)/;
+  const map = input.map(i => {
+    const valve = i.match(reg).groups;
+    valve.rate = parseInt(valve.rate);
+    return valve;
+  });
+  map.forEach(valve => {
+    valve.paths = valve.paths.split(',').map(p => _.find(map, i => i.name === p.trim()));
+  });
+
+  return map;
+};
 
 const getSolution = (input, config) => {
-  return input;
+  return 0;
 };
 
 Solver.solve(io.readLines, getValues, getSolution);
