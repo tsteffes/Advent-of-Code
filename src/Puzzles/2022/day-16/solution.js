f619251cfd18a33836bc747a650133ff04313d58
tsteffes <tim.steffes@gmail.com>
2022-12-23 08:16:02 -0500

    day 16

--- src/Puzzles/2022/day-16/solution.js
+++ src/Puzzles/2022/day-16/solution.js
@@ -3,24 +3,87 @@ const io = require('../../../helpers/io');
 const Solver = require('../../../helpers/solver');
 
 const getValues = input => {
-  const reg = /Valve (?<name>[A-Z]+) has flow rate=(?<rate>\d+); tunnels? leads? to valves? (?<paths>.*)/;
-  const map = input.map(i => {
-    const valve = i.match(reg).groups;
+  const reg = /Valve (?<name>[A-Z]+) has flow rate=(?<rate>\d+); tunnels? leads? to valves? (?<neighbors>.*)/;
+  const map = input.map(i => i.match(reg).groups);
+  return _.forEach(map, valve => {
     valve.rate = parseInt(valve.rate);
-    return valve;
+    valve.neighbors = valve.neighbors.split(',').map(p => _.find(map, i => i.name === p.trim()));
   });
-  map.forEach(valve => {
-    valve.paths = valve.paths.split(',').map(p => _.find(map, i => i.name === p.trim()));
-  });
-
-  return map;
 };
 
-const getSolution = (input, config) => {
-  return 0;
+const getNode = (name, map) => {
+  return map.find(m => m.name === name);
 };
 
-Solver.solve(io.readLines, getValues, getSolution);
+const getShortestPathLength = (path, dest, map) => {
+  let node = getNode(path[path.length - 1], map);
+  let res;
+  for (let i = 0; i < node.neighbors.length; i++) {
+    const neighbor = node.neighbors[i];
+    if (path.includes(neighbor.name)) {
+      continue;
+    }
 
-// Part 1 solution:
-// Part 2 solution:
+    let newPath = _.cloneDeep(path);
+    newPath.push(neighbor.name);
+    if (neighbor.name === dest) {
+      res = newPath;
+      break;
+    }
+
+    let p = getShortestPathLength(newPath, dest, map);
+    if (p && (!res || (res && p.length < res.length))) {
+      res = p;
+    }
+  }
+
+  return res;
+};
+
+const getMaxPath = (state, nonZeroes, globalMax) => {
+  let max = state.score;
+  const unvisited = nonZeroes.filter(m => !_.concat(...state.players.map(p => p.path)).includes(m.name));
+  let maxPossible = state.score + (state.time * _.sum(unvisited.map(u => u.rate)));
+  if (maxPossible < globalMax.max) {
+    return max;
+  }
+
+  const player = _.find(state.players, p => p.busyUntil >= state.time);
+  for (let dest of unvisited) {
+    let time = dest.paths.find(p => p.name === player.path[player.path.length - 1]).dist + 1;
+    if (time >= state.time) {
+      continue;
+    }
+
+    let newState = _.cloneDeep(state);
+    newState.score += (newState.time - time) * dest.rate;
+    newState.players[player.id].busyUntil -= time;
+    newState.players[player.id].path.push(dest.name);
+    newState.time = _.max(newState.players.map(p => p.busyUntil));
+    max = Math.max(max, getMaxPath(newState, nonZeroes, globalMax));
+    globalMax.max = Math.max(max, globalMax.max);
+  }
+
+  return max;
+};
+
+const getSolution = (map, config) => {
+  const nonZeroes = map.filter(m => m.name === 'AA' || m.rate > 0);
+  nonZeroes.forEach(start => {
+    nonZeroes.forEach(dest => {
+      const dist = dest.name === start.name ? 0 : getShortestPathLength([start.name], dest.name, map).length - 1;
+      (start.paths = start.paths || []).push({ name: dest.name, dist: dist });
+    });
+  });
+
+  const player1 = { id: 0, path: ['AA'], busyUntil: 30 - config.learningCost };
+  const player2 = { id: 1, path: ['AA'], busyUntil: config.part === 1 ? 0 : 30 - config.learningCost };
+  const state = { players: [player1, player2], score: 0, time: 30 - config.learningCost };
+  return getMaxPath(state, nonZeroes, { max: 0 });
+};
+
+const config = [{ learningCost: 0 }, { learningCost: 4 }];
+Solver.solve(io.readLines, getValues, getSolution, config);
+
+// Part 1 solution: 1923
+// Part 2 solution: 2594
