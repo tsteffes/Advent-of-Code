121334cc66d9609994bbb88b9b457a64d9247ffe
tsteffes <tim.steffes@gmail.com>
2024-12-01 10:57:24 -0500

    add template

--- advent.js
+++ advent.js
@@ -1,1 +1,8 @@
-require(`./src/Puzzles/${process.argv[2]}/Day-${process.argv[3]}/solution`);
+const fs = require('file-system');
+let file = `./src/Puzzles/${process.argv[2]}/Day-${process.argv[3]}/solution.js`;
+if (!fs.existsSync(file)) {
+  let template = fs.readFileSync('./src/Helpers/template.js');
+  fs.writeFileSync(file, template);
+}
+
+require(file);
