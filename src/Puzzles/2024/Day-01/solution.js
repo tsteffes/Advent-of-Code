1979071926fe3f5e34805a3cf34c0a3105f9971a
tsteffes <tim.steffes@gmail.com>
2024-12-02 16:12:03 -0500

    clean up day 1

--- src/Puzzles/2024/Day-01/solution.js
+++ src/Puzzles/2024/Day-01/solution.js
@@ -1,33 +1,18 @@
+require('../../../Helpers/global');
+
 const getValues = i => {
-  const first = [], second = [];
-  i.forEach(v => {
-    first.push(parseInt(v.split('   ')[0]));
-    second.push(parseInt(v.split('   ')[1]));
-  });
-  return [first, second];
+  let res = i.reduce((prev, curr) => {
+    prev[0].push(parseInt(curr.split('   ')[0]));
+    prev[1].push(parseInt(curr.split('   ')[1]));
+    return prev;
+  }, [[], []]);
+  return { len: res[0].length, a: _.sortBy(res[0]), b: _.sortBy(res[1]) };
 };
-const getSolution = (values, config) => {
-  const firstSorted = _.sortBy(values[0]);
-  const secondSorted = _.sortBy(values[1]);
-  let res = 0;
+const first = (a, b, i) => Math.abs(a[i] - b[i]);
+const second = (a, b, i) => a[i] * b.filter(v => v === a[i]).length;
+const getSolution = (obj, config) => _.sum(_.range(0, obj.len).map(i => config.alg(obj.a, obj.b, i)));
 
-  if (config.part === 1) {
-    for (let i = 0; i < firstSorted.length; i++) {
-      res += Math.abs(firstSorted[i] - secondSorted[i]);
-    }
-  }
-  else {
-    for (let i = 0; i < firstSorted.length; i++) {
-      let num = firstSorted[i];
-      let count = secondSorted.filter(v => v === num).length;
-      res += num * count;
-    }
-  }
-
-  return res;
-};
-
-Solver.solve(io.readLines, getValues, getSolution, [{ }, { }]);
+Solver.solve(io.readLines, getValues, getSolution, [{ alg: first }, { alg: second }]);
 
 // Part 1 solution:
 // Part 2 solution:
