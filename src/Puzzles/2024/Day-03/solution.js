b29f5fc59fb47ff994bca512f637bdcac38ca682
tsteffes <tim.steffes@gmail.com>
2024-12-03 08:18:10 -0500

    clean up

--- src/Puzzles/2024/Day-03/solution.js
+++ src/Puzzles/2024/Day-03/solution.js
@@ -1,13 +1,13 @@
 require('../../../Helpers/global');
 
-const regex = /(?<mul>mul\((?<p1>\d+),(?<p2>\d+)\))|(?<on>do\(\))|(?<off>don\'t\(\))/g;
-const getValues = i => [...i.join('').matchAll(regex)].map(v => v.groups);
+const reg = /(?<mul>mul\((?<p1>\d+),(?<p2>\d+)\))|(?<on>do\(\))|(?<off>don\'t\(\))/g;
+const getValues = i => [...i.join('').matchAll(reg)].map(v => v.groups);
 const getSolution = (instr, config) => {
   return instr.reduce((obj, v) => {
-    obj.sum += obj.toggle && !!v.mul ? parseInt(v.p1) * parseInt(v.p2) : 0;
-    obj.toggle = config.toggle ? !!v.on || (obj.toggle && !v.off) : true;
+    obj.sum += obj.on && v.mul ? parseInt(v.p1) * parseInt(v.p2) : 0;
+    obj.on = config.toggle ? v.on || (obj.on && !v.off) : true;
     return obj;
-  }, { sum: 0, toggle: true }).sum;
+  }, { sum: 0, on: true }).sum;
 };
 
 Solver.solve(io.readLines, getValues, getSolution, [{ toggle: false }, { toggle: true }]);
