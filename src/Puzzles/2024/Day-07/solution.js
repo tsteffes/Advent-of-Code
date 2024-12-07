f8526484add31374b919a30aeac906d89fef4f22
tsteffes <tim.steffes@gmail.com>
2024-12-07 12:27:36 -0500

    clean up

--- src/Puzzles/2024/Day-07/solution.js
+++ src/Puzzles/2024/Day-07/solution.js
@@ -6,30 +6,29 @@ const ops = [
   (a, b) => a * b,
   (a, b) => parseInt(b.toString() + a.toString())
 ];
-const getAnswers = (numOps, firstTerm, rest) => {
-  let res = [];
-  let poss = rest.length === 1 ? rest : getAnswers(numOps, rest[0], rest.slice(1));
-  poss.forEach(p => {
-    res.push(...ops.slice(0, numOps).map(op => op(firstTerm, p)));
-  });
-  return res;
+const configs = [{ ops: 2 }, { ops: 3 }];
+const parse = input => input.map(v => [...v.matchAll(reg)].map(g => {
+  return {
+    // reverse terms so that the eval occurs left-to-right
+    terms: _.reverse(g.groups.ops.split(' ').map(v => parseInt(v))),
+    ans: parseInt(g.groups.ans)
+  };
+})[0]);
+const solve = (values, config) => {
+  const solvable = v => doMath(config.ops, v.terms, v.ans).includes(v.ans);
+  return _.sum(_.filter(values, solvable).map(v => v.ans));
+};
+const doMath = (numOps, terms, ans) => {
+  const secondOperands = terms.length === 2 ? [terms[1]] : _.filter(doMath(numOps, terms.slice(1), ans), v => v <= ans);
+  return secondOperands.reduce((prev, operand) => {
+    prev.push(...ops.slice(0, numOps).map(op => op(terms[0], operand)));
+    return prev;
+  }, []);
 };
 new Puzzle(2024, 7)
-  .withParser(i => {
-    let res = i.map(v => [...v.matchAll(reg)].map(g => {
-      return {
-        ans: parseInt(g.groups.ans),
-        // reverse terms so that the eval occurs left-to-right
-        terms: _.reverse(g.groups.ops.split(' ').map(v => parseInt(v)))
-      };
-    })[0]);
-    return res;
-  })
-  .withSolver((values, config) => {
-    const solvable = v => getAnswers(config.ops, v.terms[0], v.terms.slice(1)).includes(v.ans);
-    return _.sum(_.filter(values, solvable).map(v => v.ans));
-  })
-  .solve([{ ops: 2 }, { ops: 3 }]);
+  .withParser(parse)
+  .withSolver(solve)
+  .solve(configs);
 
 // Part 1: 538191549061
 // Part 2: 34612812972206
