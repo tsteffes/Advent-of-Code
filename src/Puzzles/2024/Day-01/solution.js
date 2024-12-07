79ce117a8d2a5647d8bce694c2687952f86982c6
tsteffes <tim.steffes@gmail.com>
2024-12-07 12:57:22 -0500

    update template

--- src/Puzzles/2024/Day-01/solution.js
+++ src/Puzzles/2024/Day-01/solution.js
@@ -1,18 +1,19 @@
 require('../../../Helpers/global');
 
+const parser = i => {
+  return {
+    a: _.sortBy(i.map(v => parseInt(v.split('   ')[0]))),
+    b: _.sortBy(i.map(v => parseInt(v.split('   ')[1]))),
+    idxs: _.range(0, i.length)
+  };
+};
+const solver = (o, config) => _.sum(o.idxs.map(i => config.algo(o.a, o.b, i)));
 new Puzzle(2024, 1)
-  .withParser(i => {
-    let res = i.reduce((prev, curr) => {
-      prev[0].push(parseInt(curr.split('   ')[0]));
-      prev[1].push(parseInt(curr.split('   ')[1]));
-      return prev;
-    }, [[], []]);
-    return { len: res[0].length, a: _.sortBy(res[0]), b: _.sortBy(res[1]) };
-  })
-  .withSolver((obj, config) => _.sum(_.range(0, obj.len).map(i => config.alg(obj.a, obj.b, i))))
+  .withParser(parser)
+  .withSolver(solver)
   .solve([
-    { alg: (a, b, i) => Math.abs(a[i] - b[i]) },
-    { alg: (a, b, i) => a[i] * b.filter(v => v === a[i]).length }
+    { algo: (a, b, i) => Math.abs(a[i] - b[i]) },
+    { algo: (a, b, i) => a[i] * b.filter(v => v === a[i]).length }
   ]);
 
 // Part 1 solution: 1879048
--- src/Puzzles/2024/Day-24/solution.js
+++ src/Puzzles/2024/Day-24/solution.js
@@ -1,11 +1,15 @@
 require('../../../Helpers/global');
 
 const configs = [{}, {}];
+const parser = i => {
+  return i;
+};
+const solver = (values, config) => {
+  return 0;
+};
 new Puzzle(2024, 24)
-  .withParser(i => i)
-  .withSolver((values, config) => {
-    return 0;
-  })
+  .withParser(parser)
+  .withSolver(solver)
   .solve(configs);
 
 // Part 1 solution:
