e3ebab384df20887adf44275a2e056790108ab76
tsteffes <tim.steffes@gmail.com>
2024-12-09 16:10:45 -0500

    improve day 3

--- src/Puzzles/2024/Day-03/solution.js
+++ src/Puzzles/2024/Day-03/solution.js
@@ -1,17 +1,16 @@
 require('../../../Helpers/global');
 
-const configs = [{ toggle: false }, { toggle: true }];
 const reg = /(?<mul>mul\((?<p1>\d+),(?<p2>\d+)\))|(?<on>do\(\))|(?<off>don\'t\(\))/g;
 new Puzzle(2024, 3)
   .withParser(i => [...i.join('').matchAll(reg)].map(v => v.groups))
   .withSolver((instructions, config) => {
     return instructions.reduce((obj, instr) => {
       obj.sum += obj.on && instr.mul ? parseInt(instr.p1) * parseInt(instr.p2) : 0;
-      obj.on = config.toggle ? instr.on || (obj.on && !instr.off) : true;
+      obj.on = config.part === 2 ? instr.on || (obj.on && !instr.off) : true;
       return obj;
     }, { sum: 0, on: true }).sum;
   })
-  .solve(configs);
+  .solve();
 
 // Part 1 solution: 188192787
 // Part 2 solution: 113965544
