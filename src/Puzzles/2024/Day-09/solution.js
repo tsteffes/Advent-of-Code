98d581b623e8eef4c7f6820ab7e25d3516c9e4ad
tsteffes <tim.steffes@gmail.com>
2024-12-09 16:04:06 -0500

    day 9

--- src/Puzzles/2024/Day-09/solution.js
+++ src/Puzzles/2024/Day-09/solution.js
@@ -1,16 +1,59 @@
 require('../../../Helpers/global');
 
-const configs = [{}, {}];
-const parser = i => {
-  return i;
+const parser = input => {
+  let diskMap = input[0].split('').map(i => parseInt(i));
+  let disk = [];
+  let addr = 0;
+  for (let i = 0; i < diskMap.length; i++) {
+    let addrs = [];
+    let file = i % 2 === 0 ? { id: i / 2, addrs: addrs } : null;
+    for (let j = 0; j < diskMap[i]; j++) {
+      disk.push(file);
+      addrs.push(addr++);
+    }
+  }
+
+  return disk;
 };
-const solver = (values, config) => {
-  return 0;
+const solver = (disk, config) => {
+  if (config.part === 1) {
+    for (let i = 0; i < disk.length; i++) {
+      if (!disk[i]) {
+        let lastIndex = _.findLastIndex(disk, d => d);
+        if (lastIndex > i) {
+          let file = disk[lastIndex];
+          let index = file.addrs.pop();
+          disk[index] = null;
+          disk[i] = file;
+          file.addrs.push(i);
+          file.addrs = _.sortBy(file.addrs);
+        }
+      }
+    }
+  }
+  else {
+    const opening = (disk, size, idx) =>  _.findIndex(disk, (d, i) => !d && i < idx && (_.findIndex(disk, d => d, i) - i) >= size);
+    let files = _.reverse(_.sortBy(_.uniqWith(disk.filter(f => f), (a, b) => a.id === b.id), f => f.id));
+    files.forEach(file => {
+      let i = opening(disk, file.addrs.length, file.addrs[0]);
+      if (i >= 0) {
+        for (let x = i; x < i + file.addrs.length; x++) {
+          let index = file.addrs.pop();
+          disk[index] = null;
+          disk[x] = file;
+          file.addrs.push(x);
+          file.addrs = _.sortBy(file.addrs);
+        }
+      }
+    });
+  }
+
+  return _.sum(disk.map((d, i) => (d?.id ?? 0) * i));
 };
 new Puzzle(2024, 9)
   .withParser(parser)
   .withSolver(solver)
-  .solve(configs);
+  .solve();
 
-// Part 1 solution:
-// Part 2 solution:
+// Part 1 solution: 6463499258318
+// Part 2 solution: 6493634986625
